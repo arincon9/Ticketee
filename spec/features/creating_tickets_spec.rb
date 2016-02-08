fa65e3be689416eca12c38c9a1586d58a64bfeaf
Alex Rincon <alejandro.rincon9@gmail.com>
2016-02-07 18:20:58 -0800

    Implement creating tickets for a project

--- spec/features/creating_tickets_spec.rb
+++ spec/features/creating_tickets_spec.rb
@@ -23,4 +23,13 @@ RSpec.feature "Users can create new tickets" do
     expect(page).to have_content "Name can't be blank"
     expect(page).to have_content "Description can't be blank"
   end
+
+  scenario "with an invalid description" do
+    fill_in "Name", with: "Non-standards compliance"
+    fill_in "Description", with: "It sucks"
+    click_button "Create Ticket"
+
+    expect(page).to have_content "Ticket has not been created."
+    expect(page).to have_content "Description is too short"
+  end
 end
