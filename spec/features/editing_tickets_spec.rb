f5bd7445a9f824c4e443db53da925f93a23befd3
Alex Rincon <alejandro.rincon9@gmail.com>
2016-07-16 23:15:22 -0500

    Fix Specs for editing tickets

--- spec/features/editing_tickets_spec.rb
+++ spec/features/editing_tickets_spec.rb
@@ -1,10 +1,11 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.feature "Users can edit existing tickets" do
   let(:project) { FactoryGirl.create(:project) }
   let(:ticket) { FactoryGirl.create(:ticket, project: project) }
 
-  before do visit_project_ticket_path(project, ticket)
+  before do
+    visit project_ticket_path(project, ticket)
     click_link "Edit Ticket"
   end
 
